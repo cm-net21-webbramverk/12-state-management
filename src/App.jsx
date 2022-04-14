6ab9c193cb1fa6bed922e2dcded79ebc0048016f
David Andersson <lejonmanen@gmail.com>
2022-04-14 10:05:31 +0200

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+	const [themeIsDark, setThemeIsDark] = useState(false)
+
+	return (
+		<div className="App">
+			<header>
+			header
+			</header>
+			<main>
+			main
+			</main>
+		</div>
+
+	)
+}
+
+export default App
